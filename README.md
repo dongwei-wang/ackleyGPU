abd9ae562b0eb01d6127f69ffe902dc637179495
Dongwei <wdw828@gmail.com>
2016-10-26 23:48:30 -0400

    submission

---
+++ README.md
@@ -0,0 +1,15 @@
+# my project design
+I modified Benchmarks and F3 file
+
+my GPU implementation is in ackley_kernel.cu including the CPU implementation
+
+ackley_kernel.cu is the key file which implementation the function
+
+
+#compile
+just run "make"
+
+sh run
+
+my program could run correctly at the server
+
--- main.cpp
+++ main.cpp
@@ -32,34 +32,15 @@ double* readOvector(int dimension, int ID) {
 }
 
 int main(){
-	/*  Test the basic benchmark function */
 	double *X_CPU, *X_GPU;
-	//Benchmarks* fp=NULL;
 	F3* fp = NULL;
-
-	// struct timeval start, end;
-	// long seconds, useconds;
-	// double mtime;
 	double ackley_CPU, ackley_GPU;
-
 
 	fp = new F3();
 	X_CPU = readOvector(fp->getDimension(), fp->getID());
 	X_GPU = readOvector(fp->getDimension(), fp->getID());
-
-	//gettimeofday(&start, NULL);
 	ackley_CPU = fp->compute_CPU(X_CPU);
-	//gettimeofday(&end, NULL);
-
-	// seconds  = end.tv_sec  - start.tv_sec;
-	// useconds = end.tv_usec - start.tv_usec;
-	// mtime = (((seconds) * 1000 + useconds/1000.0) + 0.5)/1000;
-	// printf("********** CPU IMPLEMENTATION **********\n");
-	// printf("F %d value = %1.20E\n\n", fp->getID(), ackley_CPU);
-	// printf("F %d Running Time = %f s\n\n", fp->getID(), mtime);
-
 	ackley_GPU = fp->compute_GPU(X_GPU);
-
 
 	delete fp;
 	delete []X_CPU;
